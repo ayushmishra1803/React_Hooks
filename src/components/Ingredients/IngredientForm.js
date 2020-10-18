90319f812f1eb17690781f278ec028a668a9b7d3
Ayush-Mishra <ayushmishra0810@gmail.com>
2020-10-19 00:39:41 +0530

    Updates

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -30,7 +30,7 @@ const IngredientForm = React.memo((props) => {
 					</div>
 					<div className="form-control">
 						<label htmlFor="amount">Amount</label>
-						<input type="number" id="amount" value={initalState[0].amount} />
+						<input type="number" id="amount" value={initalState[0].amount}   />
 					</div>
 					<div className="ingredient-form__actions">
 						<button type="submit">Add Ingredient</button>
